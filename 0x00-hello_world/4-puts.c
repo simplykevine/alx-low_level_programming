f0def444e5332c61cc3baa3028e462d920383a25
simplykevine <umutonikevine0000@gmail.com>
2023-07-13 11:22:20 -0700

    Programmer and coder

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints "Programming is like building a multilingual
+ * puzzle, followed by a newline.
+ * Return: Always 0.
+ */
+
+int main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
