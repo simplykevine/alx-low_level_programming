bf369fbf654d6ee93cac2cef64a7d39d79951cbe
simplykevine <umutonikevine0000@gmail.com>
2023-07-14 13:17:01 -0700

    Refresh data

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	putchar('\n');
+	return (0);
+}
