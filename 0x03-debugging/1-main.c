0a099a5bc29e66cf6f02bdcb89e2c140646b6903
simplykevine <umutonikevine0000@gmail.com>
2023-07-19 09:54:44 -0700

    challenging world

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,16 +10,17 @@ int main(void)
 {
 	int i;
 
-	printf("Infinite loop incoming :(/n");
+	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-	
-	/* while (i < 10)*/
-	/*{*/
-	/*	putchar(i);*/
-	/*}*/
-	
-	printf("Infinite loop Avoided! //o/\n");
+	/**
+	 * while (i < 10)
+	 *{
+	 *	putchar(i);
+	 *}
+	 */
+	printf("Infinite loop Avoided! \\o/\n");
 
 	return (0);
 }
+
