eda8ff766478383e835dcacf498575ae18ea3e14
simplykevine <umutonikevine0000@gmail.com>
2023-07-31 08:01:55 -0700

    up to date

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_chessboard - prints the chess board
+ * @a: rows
+ *
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, b;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (b = 0; b < 8; b++)
+		{
+			_putchar(a[i][b]);
+		}
+		_putchar('\n');
+	}
+}
