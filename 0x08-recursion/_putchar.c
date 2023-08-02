618700694f4562ac5891ccd9ead9add5bf968c48
simplykevine <umutonikevine0000@gmail.com>
2023-08-02 02:27:12 -0700

    up to date

--- 0x08-recursion/_putchar.c
+++ 0x08-recursion/_putchar.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <unistd.h>
 
 /**
@@ -10,5 +11,5 @@
 
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	return(write(1, &c, 1));
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,10 +1,16 @@
-void _puts_recursion(char *s);
-void _print_rev_recursion(char *s);
-int _strlen_recursion(char *s);
+#ifndef MAIN_H
+#define MAIN_H
+
+char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
-int factorial(int n);
-int _pow_recursion(int x, int y);
-int _sqrt_recursion(int n);
-int is_prime_number(int n);
-int is_palindrome(char *s);
-int wildcmp(char *s1, char *s2);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+int _putchar(char c);
+
+#endif
