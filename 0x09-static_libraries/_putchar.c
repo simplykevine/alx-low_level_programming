d17cc8b7abd94fa119bade0ae53bd9019d1377f9
simplykevine <umutonikevine0000@gmail.com>
2023-08-05 11:04:01 -0700

    up to date

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,4 +1,6 @@
+#include "main.h"
 #include <unistd.h>
+
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -6,8 +8,9 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
+
 int _putchar(char c)
 {
-		return (write(1, &c, 1));
+	return(write(1, &c, 1));
 }
 
