ee756faab33c4ba15aba3889d07625f14f093034
simplykevine <umutonikevine0000@gmail.com>
2023-08-07 13:16:15 -0700

    up to date

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create array - create an array for characters
+ * @c: string
+ * @size: size of the array
+ * Return: 0 if successful
+ */
+char *create_array(unsigned int size, char c);
+{
+	char *buffer;
+	unsigned int position;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	/*Define values with malloc*/
+	buffer = (char *) malloc(size * sizeof(c));
+
+	if (buffer == 0)
+	{
+		return (NULL);
+	}
+
+	else
+	{
+		position = 0;
+		while (position < size) /*While for array*/
+		{
+			*(buffer + position) = c;
+			position++;
+		}
+
+		return (buffer);
+	}
+}
