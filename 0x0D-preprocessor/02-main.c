a8eebb6ae58fe1812bcc1020ef169546612c3085
simplykevine <umutonikevine0000@gmail.com>
2023-08-14 11:48:46 -0700

    up to date

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints the name of the file it was compiled.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
