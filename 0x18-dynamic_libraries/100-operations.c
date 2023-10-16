aaf1005c47562a5dc04a82c10e0ff5d5866e4032
simplykevine <umutonikevine2004@gmail.com>
2023-10-16 16:12:22 -0700

    Kevine

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,69 @@
+/**
+ * add - addition
+ * @a: number
+ * @b: nuber
+ * Return: result
+ */
+
+int add(int a, int b)
+
+{
+
+      return (a + b);
+
+}
+
+/**
+ * sub - addition
+ * @a: number
+ * @b: nuber
+ * Return: result
+ */
+
+int sub(int a, int b)
+
+{
+
+      return (a - b);
+
+}
+
+/**
+ * mul - addition
+ * @a: number
+ * @b: nuber
+ * Return: result
+ */
+
+int mul(int a, int b)
+
+{
+
+      return (a * b);
+
+}
+
+/**
+ * div - addition
+ * @a: number
+ * @b: nuber
+ * Return: result
+ */
+
+int div(int a, int b)
+
+{
+
+      return (a / b);
+
+}
+
+
+/**
+ *mod - modulo
+ *@a: number
+ */
+int mod(int a, int b)
+{
+  return (a % b);
+}
