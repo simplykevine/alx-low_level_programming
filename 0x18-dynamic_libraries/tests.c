2a2c56a76e392bd8dbf3508f430c1798d0791afa
simplykevine <umutonikevine2004@gmail.com>
2023-10-16 16:57:19 -0700

    Kevine

---
+++ 0x18-dynamic_libraries/tests.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int printf(const char *format, ...)
+{
+	write(1, "9 8 10 24 75 - 9\n",17);
+
+	write(1, "Congratulations, you win the jackpot!\n", 38);
+
+	exit(EXIT_SUCCESS);
+}
